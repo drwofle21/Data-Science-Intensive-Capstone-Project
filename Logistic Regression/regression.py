1d39ea751673cc8678d4258f6a320881eb4eef70
Oliver G <oliveragrace@gmail.com>
2016-02-04 21:12:40 -0500

    Data Science Intensive Project

--- Logistic Regression/regression.py
+++ Logistic Regression/regression.py
@@ -8,16 +8,16 @@ import math
 
 # Plot imports
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #import seaborn as sns
 #sns.set_style('whitegrid')
 
 # Machine Learning Imports
 from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
 
 # For evaluating our ML results
 from sklearn import metrics
+from sklearn.metrics import confusion_matrix, roc_curve, auc
 
 # season DataFrame is simply the regular/real seaon connected to the post/play off data
 real_season = pd.read_csv('passes.csv')
@@ -52,7 +52,6 @@ training_data['QB Rush Att'] = training_data['QB Rush Att'].apply(find_avg)
 # Remove columns to get rid of collinearity
 training_data = training_data.drop(['B.Roethlisberger','RB', 1, 'Down 4', 'PIT'], axis=1)
 
-
 # Season succesful passes
 Y_season = season['Success']
 
@@ -83,11 +82,29 @@ X_test = training_data[17822:]
 y_train = np.ravel(Y_season[:17822])
 y_test = np.ravel(Y_season[17822:])
 
+#=======================================================================================
+
 
 log_model2 = LogisticRegression()
 log_model2.fit(X_train, y_train)
 
 class_predict = log_model2.predict(X_test)
+
+def plot_confusion_matrix(df_confusion, title='Confusion matrix', cmap=plt.cm.gray_r):
+    plt.matshow(df_confusion, cmap=cmap) # imshow
+    #plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(df_confusion.columns))
+    plt.xticks(tick_marks, df_confusion.columns, rotation=45)
+    plt.yticks(tick_marks, df_confusion.index)
+    #plt.tight_layout()
+    plt.ylabel(df_confusion.index.name)
+    plt.xlabel(df_confusion.columns.name)
+
+
+df_confusion = pd.crosstab(y_test, class_predict, rownames=['Actual'], colnames=['Predicted'], margins=True)
+
+df_conf_norm = df_confusion / df_confusion.sum(axis=1)
 
 # Accuracy and null error rate
 print 'Model 2'
@@ -99,7 +116,17 @@ print '\n'
 coeff_df = DataFrame(zip(X_train.columns, np.transpose(log_model2.coef_)))
 coeff_df.to_csv('result2.csv')
 
+# Compute confusion matrix
+cm = confusion_matrix(y_test, class_predict)
+np.set_printoptions(precision=2)
+print('Confusion matrix, without normalization')
+print(cm)
 
+plot_confusion_matrix(df_confusion)
+
+fpr, tpr, thresholds = roc_curve(y_test, class_predict, pos_label=2)
+
+#======================================================================================
 log_model3 = LogisticRegression(penalty='l1')
 log_model3.fit(X_train, y_train)
 
@@ -129,3 +156,7 @@ print '\n'
 # Coeff and the impactfulness on the model
 coeff_df = DataFrame(zip(X_train.columns, np.transpose(log_model4.coef_)))
 coeff_df.to_csv('result4.csv')
+
+
+
+plt.show()
